345a4317d86bbc8744961a71e1a7c8b877536d89
keller-johnson <keller.s.johnson@gmail.com>
2022-12-20 22:50:26 -0800

    added progess bar scrub, change volume functionality

--- script.js
+++ script.js
@@ -26,15 +26,71 @@ function togglePlay() {
         showPlayIcon();
     }
 }
-
-
 // Progress Bar ---------------------------------- //
 
+//Caluclate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+//update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} / `;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
 
+//Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
 
+//volume bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    //round volume
+    if (volume < 0.15) {
+        volume = 0;
+    }
+    if (volume > 0.85) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    console.log(volume);
+
+    //change icon depending on volume
+    volumeIcon.className = '';
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+    lastVolume = volume;
+}
+
+//mute / unmute
+
+function toogleMute() {
+    if (video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+    }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -49,5 +105,9 @@ function togglePlay() {
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('ended', showPlayIcon);
-
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toogleMute);
 
